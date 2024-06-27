587d011b957b1c9438aa3bee6c9f3b8fee337463
Kamila W <kamila.wilczynska92@gmail.com>
2024-06-27 13:00:31 +0100

    add orogin netlify url address

--- app.js
+++ app.js
@@ -1,22 +1,24 @@
 const express = require('express');
 const logger = require('morgan');
-const cors = require("cors");
-
-
+const cors = require('cors');
 const booksRouter = require('./routes/Books');
 
 const app = express();
 
-app.use(cors());
+const corsOptions = {
+	origin: 'https://book-app-backend-and-frontend.netlify.app/',
+	optionsSuccessStatus: 200
+};
+
+app.use(cors(corsOptions));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.use('/books', booksRouter);
 
 app.get('/', (req, res) => {
 	res.send('Backend is running');
-})
-
-app.use('/books', booksRouter);
+});
 
 module.exports = app;
